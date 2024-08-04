4a00ae0ecead8c2e22685709a4f045392fa3ee14
Yaroslav Mudrevskyi <yarikyaroslav13200@gmail.com>
2024-08-04 15:22:57 +0300

    update Profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,34 @@
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  stats: { followers, views, likes },
+}) => {
+  return (
+    <div>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
+      </div>
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Profile;
